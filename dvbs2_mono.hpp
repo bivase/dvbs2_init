2a85d79f358d2c7981773dabfa1725be26ca1e87
bivase <bivase@bk.ru>
2020-09-09 04:11:25 +0300

    изменена функция remove_synch, добавлены новые значения класса, переименован класс

--- dvbs2_mono.hpp
+++ dvbs2_mono.hpp
@@ -1,25 +1,27 @@
-#include "stdafx.h"
+#include "stdafx/stdafx.h"
 
 template<class T = size_t>
-using dvbs2_t = typename idxs::dvbs2<T>::value_type;
+using dvbs2_mono_t = typename idxs::dvbs2_mono<T>::value_type;
 template<class T = size_t>
-constexpr T dvbs2_period_v = idxs::dvbs2<T>::period;
+constexpr T dvbs2_mono_period_v = idxs::dvbs2_mono<T>::period;
 template<class T = size_t>
-constexpr T dvbs2_len_v = idxs::dvbs2<T>::len;
+constexpr T dvbs2_mono_len_v = idxs::dvbs2_mono<T>::len;
 template<class T = size_t>
-constexpr T dvbs2_rows_v = idxs::dvbs2<T>::rows;
+constexpr T dvbs2_mono_rows_v = idxs::dvbs2_mono<T>::rows;
 template<class T = size_t>
-constexpr T dvbs2_cols_v = idxs::dvbs2<T>::cols;
+constexpr T dvbs2_mono_cols_v = idxs::dvbs2_mono<T>::cols;
 template<class T = size_t>
-using dvbs2_idxs_t = typename std::optional<std::pair<dvbs2_t<T>, dvbs2_t<T>>>;
+using dvbs2_mono_idxs_t = typename std::optional<std::pair<dvbs2_mono_t<T>, dvbs2_mono_t<T>>>;
+template<class T = size_t>
+constexpr T dvbs2_mono_pks_v = dvbs2_mono_cols_v<T> * dvbs2_mono_rows_v<T>;
 
 template<class T = size_t>
-class dvbs2 : idxs::dvbs2<T>
+class dvbs2_mono : idxs::dvbs2_mono<T>
 {
-	static constexpr auto m_idxs{ idxs::dvbs2<T>::get() };
+    static constexpr auto m_idxs{ idxs::dvbs2_mono<T>::get() };
 
-	template<class T>
-	static constexpr bool check_synch(T& data)noexcept
+    template<class V>
+    static constexpr bool check_synch(V& data)noexcept
 	{
 		return bool{
 			data[0] == 0xB8 &&
@@ -34,14 +36,14 @@ class dvbs2 : idxs::dvbs2<T>
 		};
 	}
 public:
-	static constexpr std::optional<std::pair<dvbs2_t<T>, dvbs2_t<T>>>
-		find_synch(std::vector<uint8_t>& data, dvbs2_idxs_t<T> synch_idx)noexcept
+    static constexpr std::optional<std::pair<dvbs2_mono_t<T>, dvbs2_mono_t<T>>>
+        find_synch(std::vector<uint8_t>& data, dvbs2_mono_idxs_t<T> synch_idx)noexcept
 	{
 		if (synch_idx.has_value())
 		{
 			auto r{ synch_idx.value().first };
 			auto idxs_cols{ m_idxs[0] };
-			std::array<T, dvbs2_rows_v<T>> col{};
+            std::array<T, dvbs2_mono_rows_v<T>> col{};
 			for (T idx{}; auto && elm : idxs_cols)
 			{
 				col[idx++] = shl_elm(data[elm[0]], data[elm[1]], synch_idx.value().second);
@@ -55,7 +57,7 @@ public:
 		{
 			for (T idx_bit{}; idx_bit < 8; ++idx_bit)
 			{
-				std::array<T, dvbs2_rows_v<T>> col{};
+                std::array<T, dvbs2_mono_rows_v<T>> col{};
 				for (T idx{}; auto && elm : idxs_cols)
 				{
 					col[idx++] = shl_elm(data[elm[0]], data[elm[1]], idx_bit);
@@ -70,11 +72,11 @@ public:
 		return {};
 	}
 	static constexpr void
-		remove_synch(std::vector<uint8_t>& data, std::pair<dvbs2_t<T>, dvbs2_t<T>>& synch)noexcept
+        remove_synch(std::vector<uint8_t>& data, std::pair<dvbs2_mono_t<T>, dvbs2_mono_t<T>>& synch)noexcept
 	{
 		auto col{ m_idxs[synch.first] };
 
-		auto first{ static_cast<uint8_t>(Tables::Fill(8 - synch.second)) };
+        auto first{ static_cast<uint8_t>(Tables::Fill(8 - synch.second)) };
 		auto second{ static_cast<uint8_t>(~first) };
 		data[col[0][0]] = (data[col[0][0]] & second) | (data[col[0][1]] & first);
 		data[col[1][0]] = (data[col[1][0]] & second) | (data[col[1][1]] & first);
@@ -84,7 +86,6 @@ public:
 		data[col[5][0]] = (data[col[5][0]] & second) | (data[col[5][1]] & first);
 		data[col[6][0]] = (data[col[6][0]] & second) | (data[col[6][1]] & first);
 		data[col[7][0]] = (data[col[7][0]] & second) | (data[col[7][1]] & first);
-		data[col[8][0]] = (data[col[8][0]] & second) | (data[col[8][1]] & first);
 
 		data.erase(std::next(std::begin(data), col[0][1]));
 		data.erase(std::next(std::begin(data), col[1][1] - 1));
@@ -94,6 +95,5 @@ public:
 		data.erase(std::next(std::begin(data), col[5][1] - 5));
 		data.erase(std::next(std::begin(data), col[6][1] - 6));
 		data.erase(std::next(std::begin(data), col[7][1] - 7));
-		data.erase(std::next(std::begin(data), col[8][1] - 8));
 	}
 };
